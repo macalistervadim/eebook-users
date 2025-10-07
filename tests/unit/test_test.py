8ef71578306e49b4b10ba8e859fa43c9c7afb738
macalistervadim <macalistervadim@yandex.ru>
2025-10-07 10:06:31 +0600

    fix: pre-commit secrets-alert

--- tests/unit/test_test.py
+++ tests/unit/test_test.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_dummy():
-    response = requests.get('https://httpbin.org/get', timeout=5)
+    response = requests.get('https://httpbin.org/get', timeout=10)
     assert response.status_code == 200, 'Status code is not 200'
