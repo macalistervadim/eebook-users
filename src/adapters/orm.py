4ff126079b5941303c4eb97467ac05a66dfed0f7
macalistervadim <macalistervadim@yandex.ru>
2025-10-13 10:51:03 +0600

    feat(orm-models): update orm-models with domain-model User

--- src/adapters/orm.py
+++ src/adapters/orm.py
@@ -1,19 +1,28 @@
+import datetime
 import uuid
 
-from sqlalchemy import UUID, Column, MetaData, String, Table
+from sqlalchemy import UUID, Boolean, Column, DateTime, MetaData, String, Table, func
 from sqlalchemy.orm import registry
 
 from src.domain.model import User
 
 metadata = MetaData()
 
+now = datetime.datetime.now(datetime.UTC)
 users = Table(
     'users',
     metadata,
     Column('id', UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
-    Column('first_name', String),
-    Column('last_name', String),
-    Column('email', String),
+    Column('first_name', String, nullable=False),
+    Column('last_name', String, nullable=False),
+    Column('email', String, nullable=False),
+    Column('username', String, nullable=True),
+    Column('hashed_password', String, nullable=False),
+    Column('is_active', Boolean, default=True),
+    Column('is_verified', Boolean, default=False),
+    Column('created_at', DateTime(timezone=True), server_default=func.now()),
+    Column('updated_at', DateTime(timezone=True), server_default=func.now()),
+    Column('last_login_at', DateTime(timezone=True), nullable=True),
 )
 
 
