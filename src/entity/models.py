f4365195b5a0428056604968ed70123a445e55e0
macalistervadim <macalistervadim@yandex.ru>
2025-10-17 09:50:22 +0600

    feat(repo, uow, services): add serivces, uow, repo, hasher and more for users CRUD operations (bug-ver)

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,13 +1,26 @@
-from pydantic import BaseModel
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr
 
 
-class RegisterModel(BaseModel):
+# --- Pydantic схемы ---
+class UserCreateSchema(BaseModel):
     first_name: str
     last_name: str
-    email: str
+    email: EmailStr
+    username: str
     password: str
 
 
-class LoginModel(BaseModel):
-    email: str
-    password: str
+class UserResponseSchema(BaseModel):
+    id: UUID
+    first_name: str
+    last_name: str
+    email: EmailStr
+    username: str
+    is_active: bool
+    is_verified: bool
+
+
+class ChangePasswordSchema(BaseModel):
+    new_password: str
